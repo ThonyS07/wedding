63e6bc4a29004d5d5e615f9936952ccd2f8140bd
Thony Silva <thonysilva1993@gmail.com>
2026-07-10 19:07:17 -0500

    feat: Refactor modals to use a unified Modal component and create itinerary and dress code content components
    - Created DressCodeContent component to encapsulate dress code details.
    - Refactored DressCodeModal to utilize the new Modal component and DressCodeContent.
    - Introduced ItineraryContent and ItineraryModal components for itinerary details.
    - Added Timeline component to display event timeline in the itinerary modal.
    - Updated MapModal to use the new Modal component and separate MapContent.
    - Enhanced button component to support modal actions.
    - Adjusted event details card to indicate modal usage.
    - Improved header styling to be fixed at the top of the page.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -9,6 +9,7 @@ export interface ButtonProps {
 	action: string | (() => void);
 	textColor: TextColor;
 	className?: string;
+	isModal?: boolean;
 }
 
 export const Button: React.FC<ButtonProps> = ({
@@ -17,20 +18,31 @@ export const Button: React.FC<ButtonProps> = ({
 	action,
 	textColor,
 	className: extraClassName = "",
+	isModal = false,
 }) => {
-
-	const className = `${extraClassName} ${color === "purple" && textColor === 'white'  ? `button bg-purple px-8 h-12 rounded-lg border-2 border-purple text-white inline-flex items-center justify-center cursor-pointer font-lato text-[18px]` : `button bg-transparent  px-8 h-12 rounded-lg border-2 border-purple text-purple inline-flex items-center justify-center cursor-pointer font-lato text-[18px]`}`;
+	const className = `
+		${extraClassName}
+		${
+			color === "purple" && textColor === "white"
+				? "bg-purple px-8 h-12 rounded-lg border-2 border-purple text-white"
+				: "bg-transparent px-8 h-12 rounded-lg border-2 border-purple text-purple"
+		}
+		inline-flex items-center justify-center cursor-pointer font-lato text-[18px]
+	`;
 
 	if (typeof action === "string") {
 		return (
-			<a href={action} rel='noopener noreferrer' className={className}>
+			<a
+				href={action}
+				data-modal={isModal ? "" : undefined}
+				className={className}>
 				{text}
 			</a>
 		);
 	}
 
 	return (
-		<button onClick={action} className={className}>
+		<button type='button' onClick={action} className={className}>
 			{text}
 		</button>
 	);
